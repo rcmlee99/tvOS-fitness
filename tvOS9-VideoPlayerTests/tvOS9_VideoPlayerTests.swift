d04ac309444142631395092caa7c6f7f87648832
Ethan Fan <coolioxlr@gmail.com>
2015-09-20 02:43:44 -0700

    add unit testing

---
+++ tvOS9-VideoPlayerTests/tvOS9_VideoPlayerTests.swift
@@ -0,0 +1,37 @@
+//
+//  tvOS9_VideoPlayerTests.swift
+//  tvOS9-VideoPlayerTests
+//
+//  Created by Ethan Fan on 9/20/15.
+//  Copyright © 2015 Sweefties. All rights reserved.
+//
+
+import XCTest
+
+class tvOS9_VideoPlayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testLoadingVideo() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssertTrue(true)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
